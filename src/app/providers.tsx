be6d2788d6360174d25d4c7cdb2a6f9b6ad0e286
Sheng <sisheng1998@gmail.com>
2024-12-14 16:57:32 +0800

    refactor(providers): check is client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useLocalStorage } from "usehooks-ts"
 
 import {
@@ -8,20 +8,24 @@ import {
   SIDEBAR_LOCAL_STORAGE_NAME,
   SidebarProvider,
 } from "@/components/ui/sidebar"
-import ClientOnly from "@/components/layouts/client-only"
 import ThemeProvider from "@/components/theme-provider"
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [isClient, setIsClient] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   const [sidebarState] = useLocalStorage(
     SIDEBAR_LOCAL_STORAGE_NAME,
     SIDEBAR_LOCAL_STORAGE_DEFAULT_VALUE
   )
-  const defaultOpen = sidebarState === "expanded"
 
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
-      <SidebarProvider defaultOpen={defaultOpen}>
-        <ClientOnly>{children}</ClientOnly>
+      <SidebarProvider defaultOpen={sidebarState === "expanded"}>
+        {isClient ? children : null}
       </SidebarProvider>
     </ThemeProvider>
   )
