68b627e304557d1a78d376a57ac2f80b41ee84ed
Sheng <sisheng1998@gmail.com>
2024-12-27 22:54:15 +0800

    feat(breadcrumb): display name for email page

--- src/components/layouts/header/page-breadcrumb.tsx
+++ src/components/layouts/header/page-breadcrumb.tsx
@@ -1,25 +1,35 @@
 "use client"
 
 import { usePathname } from "next/navigation"
+import { useQueryState } from "nuqs"
+import { useReadLocalStorage } from "usehooks-ts"
 
+import { Email } from "@/types/email"
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
 } from "@/components/ui/breadcrumb"
+import { EMAIL_ID_KEY, parseAsEmailId } from "@/constants/email"
 
-// TODO: Display name for the email page
 const PageBreadcrumb = (
   props: React.ComponentPropsWithoutRef<typeof Breadcrumb>
 ) => {
   const pathname = usePathname()
 
+  const [emailId] = useQueryState(EMAIL_ID_KEY, parseAsEmailId.withDefault(""))
+  const currentEmail: Email | null = useReadLocalStorage(`email-${emailId}`)
+
   return (
     <Breadcrumb {...props}>
       <BreadcrumbList>
         <BreadcrumbItem>
-          <BreadcrumbPage>{pathname === "/" ? "Home" : "Email"}</BreadcrumbPage>
+          <BreadcrumbPage className="line-clamp-1 break-all">
+            {pathname === "/" && !emailId
+              ? "Home"
+              : currentEmail?.name || "Email"}
+          </BreadcrumbPage>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
