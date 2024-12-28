6fc134e497e3892abf9921c564b273c36ba89ac4
Sheng <sisheng1998@gmail.com>
2024-12-28 22:36:52 +0800

    feat(email): update code when change email

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
+import { useQueryState } from "nuqs"
+import { useReadLocalStorage } from "usehooks-ts"
 
+import { Email } from "@/types/email"
+import { useEmail } from "@/hooks/use-email"
 import { useIsMobile } from "@/hooks/use-mobile"
+import { HOME_PAGE_CODE } from "@/components/email/constants"
 import DesktopView from "@/components/home/desktop-view"
 import MobileView from "@/components/home/mobile-view"
+import { EMAIL_ID_KEY, parseAsEmailId } from "@/constants/email"
 
 const Home = () => {
   const isMobile = useIsMobile()
+  const { setCode } = useEmail()
+
+  const [emailId] = useQueryState(EMAIL_ID_KEY, parseAsEmailId.withDefault(""))
+  const currentEmail: Email | null = useReadLocalStorage(`email-${emailId}`)
+
+  useEffect(() => {
+    setCode(currentEmail ? currentEmail.code : HOME_PAGE_CODE)
+  }, [currentEmail, setCode])
+
   return isMobile ? <MobileView /> : <DesktopView />
 }
 
