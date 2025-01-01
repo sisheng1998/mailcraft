05990bc9a17265a58394369bb5adedc6d5a0b9a2
Sheng <sisheng1998@gmail.com>
2025-01-01 08:57:40 +0800

    feat(polyfill): add polyfill for safari

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google"
+import Script from "next/script"
 
 import { cn } from "@/lib/utils"
 import Providers from "@/app/providers"
@@ -22,6 +23,14 @@ const RootLayout = ({
   children: React.ReactNode
 }>) => (
   <html lang="en" suppressHydrationWarning>
+    <head>
+      <Script
+        src="https://unpkg.com/web-streams-polyfill/dist/polyfill.js"
+        strategy="beforeInteractive"
+        integrity="sha384-btb1WLdaJTo5VvAXUjCsVnoXRjt0m2tM/dDz97AMSIasOYap1uvLAaHaQ45RQ/CZ"
+        crossOrigin="anonymous"
+      />
+    </head>
     <body
       className={cn(
         "relative flex min-h-screen flex-col overflow-x-hidden overscroll-y-none font-sans antialiased",
