de6e98976cec529b7e0e7c73164bedc88ac53cd7
Sheng <sisheng1998@gmail.com>
2025-01-01 13:19:44 +0800

    feat(email preview): add screen size indicator

--- src/components/email/email-preview.tsx
+++ src/components/email/email-preview.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import React from "react"
+import React, { useRef } from "react"
 import { useQueryState } from "nuqs"
 
 import { cn } from "@/lib/utils"
 import { useEmail } from "@/hooks/use-email"
 import { useIsMobile } from "@/hooks/use-mobile"
+import ScreenSizeIndicator from "@/components/email/screen-size-indicator"
 import PreviewContainer from "@/components/preview-container"
 import { parseAsView, VIEW_KEY, VIEWS } from "@/constants/views"
 
 const EmailPreview = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
   const isMobile = useIsMobile()
 
   const [view] = useQueryState(
@@ -21,17 +23,18 @@ const EmailPreview = () => {
 
   return (
     <PreviewContainer>
-      {previewHtml && (
-        <iframe
-          srcDoc={previewHtml}
-          title="Email Preview"
-          className={cn(
-            "h-full w-full border-0 bg-white",
-            !isMobile && view === "mobile" && "mx-auto max-w-[360px]"
-          )}
-          sandbox="allow-popups"
-        />
-      )}
+      <ScreenSizeIndicator iframeRef={iframeRef} />
+
+      <iframe
+        ref={iframeRef}
+        srcDoc={previewHtml}
+        title="Email Preview"
+        className={cn(
+          "h-full w-full border-0 bg-white",
+          !isMobile && view === "mobile" && "mx-auto max-w-[360px]"
+        )}
+        sandbox="allow-popups"
+      />
     </PreviewContainer>
   )
 }
---
+++ src/components/email/screen-size-indicator.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+
+import { cn } from "@/lib/utils"
+
+const ScreenSizeIndicator = ({
+  iframeRef,
+}: {
+  iframeRef: React.RefObject<HTMLIFrameElement>
+}) => {
+  const [dimensions, setDimensions] = useState<{
+    width: number
+    height: number
+  }>({
+    width: iframeRef.current?.clientWidth || 0,
+    height: iframeRef.current?.clientHeight || 0,
+  })
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      if (!iframeRef.current) return
+
+      setDimensions({
+        width: iframeRef.current.clientWidth,
+        height: iframeRef.current.clientHeight,
+      })
+    }
+
+    const resizeObserver = new ResizeObserver(updateDimensions)
+    iframeRef.current && resizeObserver.observe(iframeRef.current)
+
+    return () => resizeObserver.disconnect()
+  }, [iframeRef])
+
+  const { width, height } = dimensions
+
+  return (
+    <div className="flex items-center justify-center gap-2 border-b px-4 py-[0.9375rem] text-xs font-medium tracking-wider">
+      <p>
+        {width.toLocaleString()} (W) x {height.toLocaleString()} (H)
+      </p>
+
+      <span className="text-muted-foreground">-</span>
+
+      {[
+        { breakpoint: 0, label: "XS" },
+        { breakpoint: 640, label: "SM" },
+        { breakpoint: 768, label: "MD" },
+        { breakpoint: 1024, label: "LG" },
+        { breakpoint: 1280, label: "XL" },
+        { breakpoint: 1536, label: "2XL" },
+      ].map(({ breakpoint, label }, index, array) => {
+        const isVisible =
+          width >= breakpoint &&
+          (index === array.length - 1 || width < array[index + 1].breakpoint)
+
+        return (
+          <p key={label} className={cn(!isVisible && "hidden")}>
+            {label}
+          </p>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ScreenSizeIndicator
